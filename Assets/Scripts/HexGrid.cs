ef0dc85b2d6d95553b460489891522898dc44453
Francisco Carrera <franciscocarrer@gmail.com>
2018-01-18 23:49:24 -0600

    Updated spacing and offset for grid cells

--- Assets/Scripts/HexGrid.cs
+++ Assets/Scripts/HexGrid.cs
@@ -30,9 +30,9 @@ public class HexGrid : MonoBehaviour {
         // Position from the parameters using 10 as offset based on the dfault plane size
         // TO DO: Change magic numbers
         Vector3 position;
-        position.x = x * 10f;
+        position.x = (x + z * 0.5f - z / 2) * (HexMetrics.innerRadius * 2f);
         position.y = 0f;
-        position.z = z * 10f;
+        position.z = z * (HexMetrics.outerRadius * 1.5f);
 
         // Instantiate, add to HexCell array and set parent and local position 
         HexCell cell = cells[i] = Instantiate<HexCell>(cellPrefab);
